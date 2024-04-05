974d4a1b446700a895863088a059364bea0b4799
Anton Konyk <antonkonyk.com>
2024-04-05 17:32:58 +0200

    feat Create class Dish with method str
    Class Dish create with the next fields
    - name - name of a dish
    - description - description of a dish
    - price - price of a dish
    - dish_type - many to one relation with class DishType
    - cooks - many to many relation with class Cook
    - ingredients - many to many relation with class Ingredient

--- kitchen/models.py
+++ kitchen/models.py
@@ -31,3 +31,15 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    def __str__(self):
+        return {self.name}
