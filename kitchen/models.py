373127dc1fbdfc15535a9ccb1ca6299ec7784d90
Anton Konyk <antonkonyk.com>
2024-04-05 16:24:02 +0200

    feat
    Create new class DishType with class Meta ordering by name create str class presentation

--- kitchen/models.py
+++ kitchen/models.py
@@ -11,3 +11,13 @@ class Ingredient(models.Model):
         return {self.name}
 
 
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return {self.name}
+
+
