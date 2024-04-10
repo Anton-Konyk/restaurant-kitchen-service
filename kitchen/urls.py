815084eb468483dc6767deb40f7ec789b5494c44
Anton Konyk <antonkonyk.com>
2024-04-10 20:10:45 +0200

    add new paths
    add next pathes accounts/login/, accounts/password-reset/,
    accounts/register/

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen import views
+from kitchen.views import index, UserPasswordResetView, UserLoginView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("accounts/login/", UserLoginView.as_view(), name="login"),
+    path("accounts/password-reset/", UserPasswordResetView.as_view(), name="password_reset"),
+    path("accounts/register/", views.register, name="register"),
 ]
 
 app_name = "kitchen"
