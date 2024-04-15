52a1f7958f4060d0e5f7ae4bf601c05aa8c4c563
Anton Konyk <antonkonyk.com>
2024-04-15 18:41:22 +0200

    add path for logout_view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -6,6 +6,7 @@ from kitchen.views import index, UserPasswordResetView, UserLoginView
 urlpatterns = [
     path("", index, name="index"),
     path("accounts/login/", UserLoginView.as_view(), name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
     path("accounts/password-reset/", UserPasswordResetView.as_view(), name="password_reset"),
     path("accounts/register/", views.register, name="register"),
 ]
