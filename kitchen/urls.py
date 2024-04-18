d5063f7a794b2d6bb93c8baac60fdb859b5c6e4c
Anton Konyk <antonkonyk.com>
2024-04-18 14:45:19 +0200

    feat new pathes, fix sytax
    create new pathes for next views: UserPasswordChangeView, PasswordChangeDoneView,
    PasswordResetDoneView, UserPasswordResetConfirmView, PasswordResetCompleteView
    fix syntax ' to ", structure list of import views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,14 +1,42 @@
 from django.urls import path
 
 from kitchen import views
-from kitchen.views import index, UserPasswordResetView, UserLoginView
+from kitchen.views import (
+    index,
+    UserPasswordResetView,
+    UserLoginView,
+    UserPasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetDoneView,
+    UserPasswordResetConfirmView,
+    PasswordResetCompleteView,
+    IngredientListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("accounts/login/", UserLoginView.as_view(), name="login"),
     path("accounts/logout/", views.logout_view, name="logout"),
+    path("accounts/password-change/", UserPasswordChangeView.as_view(), name="password_change"),
+    path("accounts/password-change-done/", PasswordChangeDoneView.as_view(
+        template_name="accounts/password_change_done.html"
+    ), name="password_change_done"),
     path("accounts/password-reset/", UserPasswordResetView.as_view(), name="password_reset"),
+    path("accounts/password-reset-done/", PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"
+    ), name="password_reset_done"),
+    path("accounts/password-reset-confirm/<uidb64>/<token>/",
+         UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("accounts/password-reset-complete/", PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html"
+    ), name="password_reset_complete"),
     path("accounts/register/", views.register, name="register"),
+
+    path(
+        "ingredients/",
+        IngredientListView.as_view(),
+        name="ingredient-list",
+    ),
 ]
 
 app_name = "kitchen"
