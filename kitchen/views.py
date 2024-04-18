91e10d26e746b21fbe094d8318665f2442b0f13b
Anton Konyk <antonkonyk.com>
2024-04-18 13:45:44 +0200

    feat new classes fix syntax
    create new clases for work with account UserLoginView, UserPasswordResetView, PasswordResetDoneView,
    UserPasswordResetConfirmView, UserPasswordChangeView, PasswordResetCompleteView, PasswordChangeDoneView,
    create new class for view with Ingredients: IngredientListView
    and function: logout_view
    fix syntax from ' to " and delete spaces

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,19 @@
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, PasswordResetView
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView, \
+    PasswordContextMixin
+from django.shortcuts import render, redirect, resolve_url
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views import generic
+from django.views.generic import TemplateView
+from django.utils.translation import gettext as _
 
-from kitchen.forms import UserLoginForm, UserPasswordResetForm, RegistrationForm
+from kitchen.forms import UserLoginForm, UserPasswordResetForm, RegistrationForm, UserPasswordChangeForm, \
+    UserSetPasswordForm, IngredientSearchForm
 from kitchen.models import Ingredient, DishType, Cook, Dish
+from restaurant_kitchen_service import settings
 
 
 @login_required
@@ -29,27 +39,89 @@ def index(request):
     return render(request, "kitchen/index.html", context=context)
 
 
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    context_object_name = "ingredient_list"
+    template_name = "kitchen/ingredient_list.html"
+    paginate_by = 8
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IngredientListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = IngredientSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = super(IngredientListView, self).get_queryset()
+        form = IngredientSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
 class UserLoginView(LoginView):
-  template_name = "accounts/sign-in.html"
-  form_class = UserLoginForm
+    template_name = "accounts/sign-in.html"
+    form_class = UserLoginForm
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login')
 
 
 class UserPasswordResetView(PasswordResetView):
-  template_name = "accounts/password_reset.html"
-  form_class = UserPasswordResetForm
+    template_name = "accounts/password_reset.html"
+    form_class = UserPasswordResetForm
+
+
+class PasswordResetDoneView(PasswordContextMixin, TemplateView):
+    template_name = "registration/password_reset_done.html"
+    title = _("Password reset sent")
+
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = "accounts/password_reset_confirm.html"
+    form_class = UserSetPasswordForm
+
+
+class UserPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy("kitchen:password_change_done")
+    template_name = "accounts/password_change.html"
+    form_class = UserPasswordChangeForm
+
+
+class PasswordResetCompleteView(PasswordContextMixin, TemplateView):
+    template_name = "registration/password_reset_complete.html"
+    title = _("Password reset complete")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["login_url"] = resolve_url(settings.LOGIN_URL)
+        return context
+
+
+class PasswordChangeDoneView(PasswordContextMixin, TemplateView):
+    template_name = "accounts/password_change_done.html"  # registration/password_change_done.html
+    title = _("Password change successful")
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 def register(request):
-  if request.method == 'POST':
-    form = RegistrationForm(request.POST)
-    if form.is_valid():
-      form.save()
-      print("Account created successfully!")
-      return redirect('/accounts/login')
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("Account created successfully!")
+            return redirect('/accounts/login')
+        else:
+            print("Registration failed!")
     else:
-      print("Registration failed!")
-  else:
-    form = RegistrationForm()
+        form = RegistrationForm()
 
-  context = { 'form': form }
-  return render(request, 'accounts/sign-up.html', context)
+    context = {"form": form}
+    return render(request, 'accounts/sign-up.html', context)
