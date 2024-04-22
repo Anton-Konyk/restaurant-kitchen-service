4fac759b6aa0aa64b1bcadac68c22110bff5c9f1
Anton Konyk <antonkonyk.com>
2024-04-22 14:25:32 +0200

    feat create new views
    create new views for Dish Type data: DishTypeListView, DishTypeCreateView,
    DishTypeUpdateView, DishTypeDeleteView

--- kitchen/views.py
+++ kitchen/views.py
@@ -79,6 +79,49 @@ class IngredientDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy("kitchen:ingredient-list")
 
 
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "kitchen/dish_type_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishTypeListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = IngredientSearchForm(
+            initial={"name": name}
+        )
+        context["search_text"] = name
+        return context
+
+    def get_queryset(self):
+        queryset = super(DishTypeListView, self).get_queryset()
+        form = IngredientSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    template_name = "kitchen/dish_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    template_name = "kitchen/dish_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    template_name = "kitchen/dish_type_confirm_delete.html"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
 class UserLoginView(LoginView):
     template_name = "accounts/sign-in.html"
     form_class = UserLoginForm
