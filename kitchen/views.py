704da2ebf05c68db956b4aeeba4fafc6e2fd333f
Anton Konyk <antonkonyk.com>
2024-04-30 10:34:56 +0200

    create CookListView, CookCreateView, CookDetailView, CookUpdateView, toggle_assign_dish_to_cook

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView, \
-    PasswordContextMixin
+from django.contrib.auth.views import (LoginView,
+                                       PasswordResetView,
+                                       PasswordChangeView,
+                                       PasswordResetConfirmView,
+                                       PasswordContextMixin,
+                                       )
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, resolve_url
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -10,8 +15,17 @@ from django.views import generic
 from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
 
-from kitchen.forms import UserLoginForm, UserPasswordResetForm, RegistrationForm, UserPasswordChangeForm, \
-    UserSetPasswordForm, IngredientSearchForm
+from kitchen.forms import (UserLoginForm,
+                           UserPasswordResetForm,
+                           RegistrationForm,
+                           UserPasswordChangeForm,
+                           UserSetPasswordForm,
+                           IngredientSearchForm,
+                           CookSearchForm,
+                           CookCreationForm,
+                           CookForm,
+                           CookDishesSearchForm,
+                           )
 from kitchen.models import Ingredient, DishType, Cook, Dish
 from restaurant_kitchen_service import settings
 
@@ -85,7 +99,6 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
     template_name = "kitchen/dish_type_list.html"
     paginate_by = 8
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DishTypeListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
@@ -121,6 +134,90 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DishType
     template_name = "kitchen/dish_type_confirm_delete.html"
     success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    paginate_by = 8
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+        context["search_text"] = username
+        return context
+
+    def get_queryset(self):
+        queryset = super(CookListView, self).get_queryset()
+        form = CookSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
+
+
+class CookCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Cook
+    form_class = CookCreationForm
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.all().prefetch_related("dishes__dish_type")
+    paginate_by = 8
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookDetailView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = CookDishesSearchForm(
+            initial={"name": name}
+        )
+        context["search_text"] = name
+        return context
+
+    def get_queryset(self):
+        queryset = super(CookDetailView, self).get_queryset()
+        print(queryset)
+        form = CookDishesSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+"""
+    def get_queryset(self):
+        # queryset = super(CookDetailView, self).get_queryset()
+        queryset = Cook.objects.get(id=self.kwargs["pk"]).dishes.all()
+        form = CookDishesSearchForm(self.request.GET)
+
+        if form.is_valid():
+            print(queryset.filter(name__icontains=form.cleaned_data["name"]))
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset"""
+
+
+class CookUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Cook
+    form_class = CookForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        cook_instance = form.instance
+        dishes = form.cleaned_data['dishes']
+        cook_instance.dishes.clear()
+        for dish in dishes:
+            cook_instance.dishes.add(dish)
+        return response
+
+
+class CookDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Cook
+    success_url = reverse_lazy("")
 
 
 class UserLoginView(LoginView):
@@ -165,7 +262,7 @@ class PasswordResetCompleteView(PasswordContextMixin, TemplateView):
 
 
 class PasswordChangeDoneView(PasswordContextMixin, TemplateView):
-    template_name = "accounts/password_change_done.html"  # registration/password_change_done.html
+    template_name = "accounts/password_change_done.html"
     title = _("Password change successful")
 
     @method_decorator(login_required)
@@ -187,3 +284,15 @@ def register(request):
 
     context = {"form": form}
     return render(request, 'accounts/sign-up.html', context)
+
+
+@login_required
+def toggle_assign_dish_to_cook(request, pk, cook_id):
+    cook = Cook.objects.get(id=cook_id)
+    if (
+        Dish.objects.get(id=pk) in cook.dishes.all()
+    ):
+        cook.dishes.remove(pk)
+    # else:
+    #     cook.dishes.add(pk)
+    return HttpResponseRedirect(reverse_lazy("kitchen:cook-detail", args=[cook.id]))
