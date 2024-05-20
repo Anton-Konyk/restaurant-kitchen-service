3002d020a03de42ae270d181eb5b543934290d0e
Anton Konyk <antonkonyk.com>
2024-05-20 18:31:48 +0200

    add getenv for AALLOWED_HOSTS revove default password for SECRET_KEY

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-q=p#*b^^w%$ao3ceo-@uzjg5n0cc4()&z+(w64n&c7y8ga6pqj")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
